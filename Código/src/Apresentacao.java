6196710c64a76db5f6718bfa6b720a647cb6368a
gramos22 <gabrielrmzf@gmail.com>
2023-10-06 08:36:18 -0300

    🚀: generate getters for Apresentacao class

--- Código/src/Apresentacao.java
+++ Código/src/Apresentacao.java
@@ -4,4 +4,11 @@ public class Apresentacao {
     private Time horaApresentacao;
     private Trabalho trabalho;
 
+    public Time getHoraApresentacao() {
+        return horaApresentacao;
+    }
+    public Trabalho getTrabalho() {
+        return trabalho;
+    }
+
 }
