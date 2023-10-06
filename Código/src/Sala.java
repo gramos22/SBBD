31926856213b4129fbc43170972efd4afbd5a692
gramos22 <gabrielrmzf@gmail.com>
2023-10-06 13:02:57 -0300

    🚀: generate getters and setters for Sala class

--- Código/src/Sala.java
+++ Código/src/Sala.java
@@ -3,4 +3,23 @@ public class Sala {
     private int idPredio;
     private int capacidade;
     
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public int getIdPredio() {
+        return idPredio;
+    }
+    public void setIdPredio(int idPredio) {
+        this.idPredio = idPredio;
+    }
+    public int getCapacidade() {
+        return capacidade;
+    }
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
 }
