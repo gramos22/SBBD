ba94fe51f166221879a9e90bb889585c14a17124
gramos22 <gabrielrmzf@gmail.com>
2023-10-06 14:28:57 -0300

    🚀: generate getters and setters for Empresa class

--- Código/src/Empresa.java
+++ Código/src/Empresa.java
@@ -1,3 +1,11 @@
 public class Empresa extends Instituicao {
     private String ramoAtividade;
+
+    public String getRamoAtividade() {
+        return ramoAtividade;
+    }
+
+    public void setRamoAtividade(String ramoAtividade) {
+        this.ramoAtividade = ramoAtividade;
+    }
 }
