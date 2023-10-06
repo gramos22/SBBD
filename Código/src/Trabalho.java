84f899f619d25a9c037fb695c212b25e77718c99
gramos22 <gabrielrmzf@gmail.com>
2023-10-06 15:24:40 -0300

    🚀: add listarIntegrantes() & adicionarIntegrante()

--- Código/src/Trabalho.java
+++ Código/src/Trabalho.java
@@ -23,5 +23,34 @@ public class Trabalho {
         return integrantes;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void setOrientador(Professor orientador) {
+        this.orientador = orientador;
+    }
+    public void setIntegrantes(ArrayList<Estudante> integrantes) {
+        this.integrantes = integrantes;
+    }
+    public void listarIntegrantes() {
+        int tamIntegrantes = integrantes.size();
+
+        System.out.println("Integrantes:");
+        for (int i = 0; i < tamIntegrantes; i++) {
+            System.out.println("- " + integrantes.get(i).getNome());
+        }
+
+    }
+
+    public void adicionarIntegrante(Estudante estudante) {
+        integrantes.add(estudante);
+    }
+
     
 }
