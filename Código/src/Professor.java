1051b099d02e6de2033fca0fcb3ee6b3f12c2a64
EstevaoFR10 <rodriguesfaria35@gmail.com>
2023-10-06 15:13:46 -0300

    Estevao submenus

--- Código/src/Professor.java
+++ Código/src/Professor.java
@@ -1,3 +1,13 @@
 public class Professor extends Pessoa {
     private String disciplina;
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    
 }
