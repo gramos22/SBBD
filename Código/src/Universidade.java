0a728ce2d607640269355ddefee5f6ed2e0b9614
gramos22 <gabrielrmzf@gmail.com>
2023-10-06 14:35:14 -0300

    🚀: generate getters and setters for Universidade class

--- Código/src/Universidade.java
+++ Código/src/Universidade.java
@@ -1,3 +1,13 @@
 public class Universidade extends Instituicao {
     private int qtdeCursos;
+
+    public int getQtdeCursos() {
+        return qtdeCursos;
+    }
+
+    public void setQtdeCursos(int qtdeCursos) {
+        this.qtdeCursos = qtdeCursos;
+    }
+
+    
 }
