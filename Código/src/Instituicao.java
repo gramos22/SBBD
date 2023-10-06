92d198cb35c48e8c1f479e644e20141e56f2da3f
gramos22 <gabrielrmzf@gmail.com>
2023-10-06 09:43:40 -0300

    🚀: generate getters for Instituicao class

--- Código/src/Instituicao.java
+++ Código/src/Instituicao.java
@@ -1,4 +1,11 @@
 abstract class Instituicao {
     private String cnpj;
     private String nome;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+    public String getNome() {
+        return nome;
+    }
 }
